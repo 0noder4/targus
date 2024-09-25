bd4acb845bc9a877745422f7820bbe4fc8202f72
0noder4 <kuklewskibartosz@gmail.com>
2024-09-25 16:58:57 +0200

    refactor: migration to typescript

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/icon.ico" />
+      </Head>
+      Inżynierskie Targi Pracy WEB
+    </div>
+  );
+}
