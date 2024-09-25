8179d974c6e9c97e1b6bbe021d44f78d10d490f6
0noder4 <kuklewskibartosz@gmail.com>
2024-09-25 10:45:52 +0200

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Inżynierskie Targi Pracy 2024</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Inżynierskie targi pracy WEB</main>
+    </div>
+  );
+}
