f9f7b06d7f4ebbb7fd4e819a3acb67c8b1ada042
0noder4 <kuklewskibartosz@gmail.com>
2024-10-01 18:53:58 +0200

    feat/refactor: changed file structure, font imports, global style definitions, basic routing, header component

---
+++ frontend/app/business/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Head from "next/head";
+
+export const metadata: Metadata = {
+  title: "Inżynierskie Targi Pracy 2025",
+  description: "Największe Inżynierskie Targi Pracy w Polsce",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pl">
+      <Head>
+        <link rel="icon" href="/icon.ico" />
+      </Head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/main/Header/Header";
+import React from "react";
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Index;
