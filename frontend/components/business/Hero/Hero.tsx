525535d4173b0cd76888a58aea2a145a6cbcf687
0noder4 <kuklewskibartosz@gmail.com>
2024-12-30 13:45:33 +0100

    feat: business hero section

--- frontend/components/business/Hero/Hero.tsx
+++ frontend/components/business/Hero/Hero.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import "./Hero.scss";
+import Aside from "./components/Aside/Aside";
+
+import FOCUS from "@/public/figures/itp-figure--business_focus.svg";
+import BARCODE from "@/public/figures/itp-figure--barcode.svg";
+import Image from "next/image";
+
 const Hero = () => {
-  return <section className="itp-c-section--hero">Hero</section>;
+  return (
+    <section className="itp-business_section--hero">
+      <Aside />
+      <div className="itp-c-hero__focus">
+        <label className="itp-c-hero__focus__tl">
+          <span>
+            11/13/
+            <br />
+            2025
+          </span>
+          <Image src={BARCODE} alt="Barcode" />
+        </label>
+        <figure className="itp-c-hero__focus__figure">
+          <Image src={FOCUS} alt="Business focus" />
+        </figure>
+        <label className="itp-c-hero__focus__br">
+          odkrywamy <br /> nowe talenty
+        </label>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
---
+++ frontend/components/business/Hero/components/Aside/Aside.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./Aside.scss";
+
+function Aside() {
+  return (
+    <aside className="itp-business_section__aside">
+      <div className="itp-business_section__aside__letter">3</div>
+      <div className="itp-business_section__aside__letter">1</div>
+      <div className="itp-business_section__aside__letter">.</div>
+      <div className="itp-business_section__aside__letter">i</div>
+      <div className="itp-business_section__aside__letter">n</div>
+      <div className="itp-business_section__aside__letter">ż</div>
+      <div className="itp-business_section__aside__letter">y</div>
+      <div className="itp-business_section__aside__letter">n</div>
+      <div className="itp-business_section__aside__letter">i</div>
+      <div className="itp-business_section__aside__letter">e</div>
+      <div className="itp-business_section__aside__letter">r</div>
+      <div className="itp-business_section__aside__letter">s</div>
+      <div className="itp-business_section__aside__letter">-</div>
+      <div className="itp-business_section__aside__letter">k</div>
+      <div className="itp-business_section__aside__letter">i</div>
+      <div className="itp-business_section__aside__letter">e</div>
+      <div className="itp-business_section__aside__letter">t</div>
+      <div className="itp-business_section__aside__letter">a</div>
+      <div className="itp-business_section__aside__letter">r</div>
+      <div className="itp-business_section__aside__letter">g</div>
+      <div className="itp-business_section__aside__letter">i</div>
+      <div className="itp-business_section__aside__letter">p</div>
+      <div className="itp-business_section__aside__letter">r</div>
+      <div className="itp-business_section__aside__letter">a</div>
+      <div className="itp-business_section__aside__letter">c</div>
+      <div className="itp-business_section__aside__letter">y</div>
+    </aside>
+  );
+}
+
+export default Aside;
