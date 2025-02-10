eda4d589fc41fc9b6e9eeac1722068e6b425d868
0noder4 <kuklewskibartosz@gmail.com>
2025-02-10 21:51:56 +0100

    feat: improved metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,9 +3,21 @@ import "/styles/scss/index.scss";
 import "/styles/css/index.css";
 
 export const metadata: Metadata = {
+  generator: "Next.js",
+  metadataBase: new URL("https://targipracy.org.pl"),
   title: "Inżynierskie Targi Pracy 2025",
+  keywords: ["Targi pracy", "Politechnika", "Staż", "Praca", "Politechnika"],
+  authors: [{ name: "0noder4" }],
+  creator: "Bartosz Kuklewksi",
   description:
     "31. Inżynierskie Targi Pracy – największe wydarzenie rekrutacyjne dla inżynierów i studentów kierunków technicznych na Politechnice Warszawskiej! Spotkaj czołowych pracodawców, znajdź staż, praktyki lub wymarzoną pracę. Dołącz 11-12 marca na Politechnice Warszawskiej i rozwijaj swoją karierę!",
+  openGraph: {
+    title: "Inżynierskie Targi Pracy 2025",
+    description: "Napędź swoją karierę",
+    url: "https://targipracy.org.pl",
+    siteName: "Inżynierskie Targi Pracy",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
