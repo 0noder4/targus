ab6d6a942dc172bceaa65c71baeaf4e4885f342b
0noder4 <kuklewskibartosz@gmail.com>
2025-03-03 15:09:44 +0100

    feat: ditch manual content policy, added nonces

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com *.usercentrics.eu;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    connect-src 'self' https://api.targipracy.org.pl/ http://localhost:1337/ *.google-analytics.com;
-    upgrade-insecure-requests;
-    script-src-elem 'self' 'unsafe-eval' 'unsafe-inline' *.usercentrics.eu *.google-analytics.com *.googletagmanager.com;
-`;
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -27,19 +12,6 @@ const nextConfig = {
         search: "",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
   },
 };
 
